e3d1c029ba1ecec3cb696cb7a1a759880f68cd5e
LemonHaque <azizulhaquelemon31@gmail.com>
2023-05-26 12:36:57 +0600

    added menu category by using custom hook

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../Hooks/UseMenu";
 
 const PopularMenu = () => {
+const [menu] = useMenu();
+const popular = menu.filter(item => item.category === 'popular')
+    // const [menu, setMenu] = useState([]);
 
-    const [menu, setMenu] = useState([]);
-
-    useEffect(() => {
-        fetch('menu.json')
-            .then((response) => response.json())
-            .then((data) =>{
-                const popularItems = data.filter(item => item.category === 'popular')
-                 setMenu(popularItems)})
-    }, [])
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             const popularItems = data.filter(item => item.category === 'popular')
+    //             setMenu(popularItems)
+    //         })
+    // }, [])
 
     return (
         <section>
@@ -23,15 +26,15 @@ const PopularMenu = () => {
 
             <div className="grid md:grid-cols-2 gap-8 px-6">
                 {
-                    menu.map(item => <MenuItem 
-                    key={item._id}
-                    item={item}
-                    ></MenuItem> )
+                    popular.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
                 }
             </div>
-           <div className="text-center">
-           <button className='btn btn-outline border-0 border-b-4 mt-6'>View Full Menu</button>
-           </div>
+            <div className="text-center">
+                <button className='btn btn-outline border-0 border-b-4 mt-6'>View Full Menu</button>
+            </div>
 
         </section>
     );
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,63 @@
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg"
+import saladImg from "../../../assets/menu/salad-bg.jpg"
+import soupImg from "../../../assets/menu/soup-bg.jpg"
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg"
+import useMenu from '../../../Hooks/UseMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
+
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert')
+    const soups = menu.filter(item => item.category === 'soup')
+    const salads = menu.filter(item => item.category === 'salad')
+    const pizzas = menu.filter(item => item.category === 'pizza')
+    const offered = menu.filter(item => item.category === 'offered')
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>Menu</h2>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+
+            {/* main cover */}
+            <SectionTitle
+                subHeading={"Don't Miss"} heading={"Today's Offer"}
+            ></SectionTitle>
+            {/* offered menu items */}
+            <MenuCategory
+                items={offered}
+            ></MenuCategory>
+
+            {/* desert menu items */}
+            <MenuCategory
+                items={desserts}
+                title="Desserts"
+                img={dessertImg}
+            ></MenuCategory>
+
+            {/* pizza menu items */}
+            <MenuCategory
+                items={pizzas}
+                title="Pizzas"
+                img={pizzaImg}
+            ></MenuCategory>
+            {/* slalad menu items */}
+            <MenuCategory
+                items={salads}
+                title="Salads"
+                img={saladImg}
+            ></MenuCategory>
+            {/* pizza menu items */}
+            <MenuCategory
+                items={soups}
+                title="Soups"
+                img={soupImg}
+            ></MenuCategory>
         </div>
     );
 };
