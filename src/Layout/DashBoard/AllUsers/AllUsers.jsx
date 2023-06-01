3ad53b8d08e6ff4604ee5e67db5a06724b369300
LemonHaque <azizulhaquelemon31@gmail.com>
2023-06-01 23:48:59 +0600

    added jwt token and save on it client side

--- src/Layout/DashBoard/AllUsers/AllUsers.jsx
+++ src/Layout/DashBoard/AllUsers/AllUsers.jsx
@@ -1,12 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
-import { FaTrashAlt } from "react-icons/fa";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const { data: users = [], refetch } = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
     })
+
+    const handleMakeAdmin = user => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH',
+
+        })
+        .then(res => res.json())
+          .then(data=>{
+            console.log(data);
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+          })  
+    }
 
     const handleDelete = user =>{
 
@@ -36,7 +58,7 @@ const AllUsers = () => {
                                 <th>{index + 1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td>Admin</td>
+                                <td>{user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className="btn btn-warning text-white bg-yellow-500"><FaUserShield></FaUserShield></button>}</td>
                                 <td><button onClick={() => handleDelete(user)} className="btn btn-error text-white bg-red-600"><FaTrashAlt></FaTrashAlt></button></td>
                             </tr>)
                         }
